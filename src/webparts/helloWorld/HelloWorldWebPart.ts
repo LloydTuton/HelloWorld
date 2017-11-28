de2f98e758f2e8b7c5569c21101ea08bc5efbb7d
LloydTuton <32952482+LloydTuton@users.noreply.github.com>
2017-11-28 14:34:37 +0000

    First commit

--- src/webparts/helloWorld/HelloWorldWebPart.ts
+++ src/webparts/helloWorld/HelloWorldWebPart.ts
@@ -44,44 +44,46 @@ export default class HelloWorldWebPartWebPart extends BaseClientSideWebPart<IHel
     </div>`;
   }
 
-    protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
-      return {
-        pages: [
+
+protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+  return {
+    pages: [
+      {
+        header: {
+          description: strings.PropertyPaneDescription
+        },
+        groups: [
           {
-            header: {
-              description: strings.PropertyPaneDescription
-            },
-            groups: [
-              {
-                groupName: strings.BasicGroupName,
-                groupFields: [
-                PropertyPaneTextField('description', {
-                  label: 'Description'
-                }),
-                PropertyPaneTextField('test', {
-                  label: 'Multi-line Text Field',
-                  multiline: true
-                }),
-                PropertyPaneCheckbox('test1', {
-                  text: 'Checkbox'
-                }),
-                PropertyPaneDropdown('test2', {
-                  label: 'Dropdown',
-                  options: [
-                    { key: '1', text: 'One' },
-                    { key: '2', text: 'Two' },
-                    { key: '3', text: 'Three' },
-                    { key: '4', text: 'Four' }
-                  ]}),
-                PropertyPaneToggle('test3', {
-                  label: 'Toggle',
-                  onText: 'On',
-                  offText: 'Off'
-                })
-              ]
-              }
-            ]
+            groupName: strings.BasicGroupName,
+            groupFields: [
+            PropertyPaneTextField('description', {
+              label: 'Description'
+            }),
+            PropertyPaneTextField('test', {
+              label: 'Multi-line Text Field',
+              multiline: true
+            }),
+            PropertyPaneCheckbox('test1', {
+              text: 'Checkbox'
+            }),
+            PropertyPaneDropdown('test2', {
+              label: 'Dropdown',
+              options: [
+                { key: '1', text: 'One' },
+                { key: '2', text: 'Two' },
+                { key: '3', text: 'Three' },
+                { key: '4', text: 'Four' }
+              ]}),
+            PropertyPaneToggle('test3', {
+              label: 'Toggle',
+              onText: 'On',
+              offText: 'Off'
+            })
+          ]
           }
         ]
-      };
-    }
+      }
+    ]
+  };
+}
+}
